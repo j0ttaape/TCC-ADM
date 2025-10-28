ec68bf48d09eaa79d3ab7ef47c44660af4d66790
Tukis-bit <arthur01raphael@gmail.com>
2025-10-28 09:18:06 -0300

    estoque inserindo automaticamente

--- backend/src/repository/cadastrar_hemo/cadastrarHemoRepository.js
+++ backend/src/repository/cadastrar_hemo/cadastrarHemoRepository.js
@@ -9,5 +9,20 @@ export default async function cadastrarHemocentro(informacoes){
     `
 
     const [info] = await connection.query(comando,[informacoes.nome_hemocentro,informacoes.cidade_hemocentro,informacoes.bairro_hemocentro,informacoes.rua_hemocentro])
+
+    const comando2 = `
+    INSERT INTO estoque (tipo_sanguineo, quantidade_bolsas, quantidade_maxima, id_hemocentro)
+    VALUES
+    ('A+', 0, 0, ?),
+    ('A-', 0, 0, ?),
+    ('B+', 0, 0, ?),
+    ('B-', 0, 0, ?),
+    ('AB+', 0, 0, ?),
+    ('AB-', 0, 0, ?),
+    ('O+', 0, 0, ?),
+    ('O-', 0, 0, ?);
+
+    `
+    await connection.query(comando2,[info.insertId,info.insertId,info.insertId,info.insertId,info.insertId,info.insertId,info.insertId,info.insertId])
     return info.insertId
 }
